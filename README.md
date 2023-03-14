418e1339595b2400709fdfa2cca8bf016691c37c
ByteOtter <christopher-hock@protonmail.com>
2023-03-14 16:50:39 +0100

    README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# nester
+# Nester
+
+![./index_logo.png]
+
 Create project structure for Python, C#, C++, Java and Ruby
---
+++ nester.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# USAGE: nester <OPERATION> <LANGUAGE> <PATH> <FLAG>
+# EXAMPLE: nester create python . -s | Create src-layout for python project in
+#                                      current directory.
+
+languages=("python", "cs", "cpp", "java", "ruby")
+
